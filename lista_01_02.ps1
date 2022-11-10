e9a6fb7aa87ef5d48623ead2d745954ef230920b
Gabriel Pires <60672959+piresp@users.noreply.github.com>
2022-11-10 15:48:04 -0300

    Create lista_01_02.ps1

---
+++ lista_01_02.ps1
@@ -0,0 +1,11 @@
+# 2 - Calcular o dobro da area de um quadrado
+
+Clear-Host
+
+[float]$lado = Read-Host " Insira o valor do lado de um quadrado"
+
+[float]$area = $lado * $lado
+
+[float]$dobro = $area * 2
+
+" Area = $area Dobro da Area = $dobro "
